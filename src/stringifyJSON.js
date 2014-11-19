c64e9db7ca372fc168316f76566ae83eef8c4188
Philip Rosen <philipjrosen@gmail.com>
2014-11-19 08:59:42 -0800

    stringifies numbers

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,5 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'number') return obj.toString();
 };
