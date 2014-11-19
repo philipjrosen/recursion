9d0bea37c2e37bf87ac5db094db8d3fd500495ad
Philip Rosen <philipjrosen@gmail.com>
2014-11-19 09:50:38 -0800

    stringifies arrays and nested arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,5 +6,5 @@ var stringifyJSON = function(obj) {
   if (typeof obj === 'number' || typeof obj === 'boolean') return obj.toString();
   if (obj === null) return "null";
   if (typeof obj === 'string') return '"' + obj + '"';
-  if (Array.isArray(obj)) return "[]";
+  if (Array.isArray(obj)) return "[" + obj.map(function(elem) { return stringifyJSON(elem);}).join(",") + "]";
 };
